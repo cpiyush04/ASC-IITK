d66b73d5eecc4bcfc3f676f2e6535b1c818eaff9
piyush chandra <piyushchandra004@gmail.com>
2023-06-04 11:57:46 +0530

    Adventure sports club
    Website template demo

---
+++ script.js
@@ -0,0 +1,33 @@
+ function openNav(){
+      document.getElementById("myNav").style.width = "100%";
+   }
+
+   function openNav1(){
+      document.getElementById("Nav1").style.width = "100%";
+   }
+
+   function openNav2(){
+      document.getElementById("Nav2").style.width = "100%";
+   }
+
+   function closeNav(){
+      document.getElementById("myNav").style.width = "0%";
+   }
+
+   function closeNav1(){
+      document.getElementById("Nav1").style.width = "0%";
+   }
+   function closeNav2(){
+      document.getElementById("Nav2").style.width = "0%";
+   }
+
+   function opSearch(){
+      let bar = document.getElementById("mySearch");
+      bar.style.display="flex";
+   }
+
+   function reset(){
+      let bar = document.getElementById("mySearch");
+      bar.style.display="none";
+   }
+
